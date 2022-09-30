f48c6885931574483838e2cfc6b949155712f397
rico4pepe <rico4pepe@gmail.com>
2022-09-30 14:53:02 +0100

    Laravel framework with debugbar installed to test code in development environment

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Barryvdh\Debugbar\Facades\Debugbar;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    Debugbar::info('INFO'); // calling the debugbar class before returning view 
+    return view('welcome');
+});
